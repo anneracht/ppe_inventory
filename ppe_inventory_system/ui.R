556eef2d089be11092ea4d0477f1c54459743324
anneracht <anneracht@gmail.com>
2020-03-31 14:01:46 -0400

    stuff

--- ppe_inventory_system/ui.R
+++ ppe_inventory_system/ui.R
@@ -1,33 +1,73 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+
+  headerPanel(span("Inventory Management System", style = "color:blue")),br(),
+
+
+  selectInput(inputId = "product_id", label = h4("Select Product:"),
+              c("Washington Berry Juice" = 1,
+                "Washington Mango Drink" = 2,
+                "Washington Strawberry Drink" = 3,
+                "Washington Cream Soda" = 4,
+                "Washington Diet Soda" = 5,
+                "Washington Cola" = 6,
+                "Washington Diet Cola"= 7,
+                "Washington Orange Juice" = 8,
+                "Washington Cranberry Juice" = 9)), br(),
+
+
+  titlePanel(h3(textOutput("product_text"))), br(),
+
+  sidebarPanel(
+    radioButtons("method", h4( "Forecast Technique: ", style = "color:blue"),
+                 c("Naive" = "naive",
+                   "Moving Average" = "ma",
+                   "Exponential Smoothing" = "es")),
+    br(),
+
+    h4("Calculation", style = "color:blue"), br(),
+
+    strong(textOutput("lead_time")),br(),
+
+    strong(textOutput("safety_stock")),br(),
+
+    strong(textOutput("reorder_point")),br(),
+    class = 'leftAlign'
+  ),
+
+  sidebarPanel(
+    plotOutput("product_plot"),
+    width = 8,
+    class = 'leftAlign'
+  ),
+
+
+  mainPanel(
+
+
+
+    tabsetPanel(type = "tabs",
+                tabPanel("Forecast",
+                         strong("Naive"),verbatimTextOutput("forecast_naive_output"),
+                         strong("Moving Average"),verbatimTextOutput("forecast_sma_output"),
+                         strong("Exponential Smoothing"),verbatimTextOutput("forecast_es_output")
+                ),
+                tabPanel("Error rates",
+                         strong("Naive"),verbatimTextOutput("forecast_naive_accuracy"),
+                         strong("Moving Average"), verbatimTextOutput("forecast_sma_accuracy"),
+                         strong("Exponential Smoothing"), verbatimTextOutput("forecast_es_accuracy")
+                ),
+                tabPanel("Plots",
+                         plotOutput("naive_plot"),
+                         plotOutput("sma_plot"),
+                         plotOutput("es_plot"),
+                         class = 'rightAlign'),
+                tabPanel("Data",
+                         dataTableOutput("product_dataHead")
+                )
     )
   )
-))
+)
+)
